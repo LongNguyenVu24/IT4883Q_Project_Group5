28e3fff10eca3431104864acbef35c3b96eec2ef
Mạnh Hùng <81520197+hungnm12@users.noreply.github.com>
2023-04-26 22:17:21 +0700

    update comp

---
+++ WorkManage/src/app/components/subtasks/subtasks.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-subtasks',
+  templateUrl: './subtasks.component.html',
+  styleUrls: ['./subtasks.component.css']
+})
+export class SubtasksComponent {
+
+}
