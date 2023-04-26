5de2d3bbe409446f3f43da546c174cf8124350db
Mạnh Hùng <81520197+hungnm12@users.noreply.github.com>
2023-04-26 22:21:13 +0700

    update comp 3rd

---
+++ WorkManage/src/app/components/lilgantt/lilgantt.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lilgantt',
+  templateUrl: './lilgantt.component.html',
+  styleUrls: ['./lilgantt.component.css']
+})
+export class LilganttComponent {
+
+}
