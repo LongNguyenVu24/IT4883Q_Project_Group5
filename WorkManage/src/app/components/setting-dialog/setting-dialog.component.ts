57d7c3baf335eca5893b49baa9b8c1484ccd45c5
LeThanhDat <89300362+lethanhdathust@users.noreply.github.com>
2023-07-04 22:18:16 +0700

    update pw and user

--- WorkManage/src/app/components/setting-dialog/setting-dialog.component.ts
+++ WorkManage/src/app/components/setting-dialog/setting-dialog.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 import { MatDialogRef } from '@angular/material/dialog';
@@ -7,10 +8,38 @@ import { MatDialogRef } from '@angular/material/dialog';
   styleUrls: ['./setting-dialog.component.css']
 })
 export class SettingDialogComponent {
+  private baseUrl = 'http://localhost:8003/api';
+ 
+
   activeTab = 'Account';
+  email: string = '';
+  newPassword: string = '';
+  newFullName: string = ''; 
 
-  constructor(public dialogRef: MatDialogRef<SettingDialogComponent>) { }
+  constructor(public dialogRef: MatDialogRef<SettingDialogComponent>,private http: HttpClient) { }
+  async changePassword() {
+    console.log(1)
+    let res =await this.changeInformation().toPromise().then(() => {
+       this.closeDialog();
 
+
+    }).catch((e:any) => {
+      this.closeDialog();
+
+      console.log(e)
+    });
+    this.closeDialog();
+   
+    }
+  changeInformation() {
+    const newInformation = {
+      email:this.email,
+      newPassword: this.newPassword,
+      newFullname: this.newFullName
+    };
+    console.log(123)
+    return this.http.post('http://localhost:8003/api/v1/auth/updatePassword',newInformation);
+  }
   closeDialog() {
     this.dialogRef.close();
   }
